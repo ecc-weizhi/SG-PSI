bb486fc047b92ffe8dcb7fd330b49e8b40b15364
ecc-weizhi <ecc.weizhi@gmail.com>
2017-01-30 15:23:54 +0800

    Display google map in activity
    Rename MainActivity to MapActivity. MapActivity now display google map.

---
+++ app/src/main/java/com/example/weizhi/sg_psi/ui/map/MapActivity.java
@@ -0,0 +1,98 @@
+package com.example.weizhi.sg_psi.ui.map;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.weizhi.sg_psi.R;
+import com.example.weizhi.sg_psi.SgPsiApplication;
+import com.example.weizhi.sg_psi.data.PsiDataSource;
+import com.example.weizhi.sg_psi.network.response.PsiJson;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        SgPsiApplication.getPsiRepository().getPsi(new PsiDataSource.GetPsiCallback() {
+            @Override
+            public void onPsiLoaded(@NonNull PsiJson psiJson) {
+                Toast.makeText(MapActivity.this, "got data", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDataNotAvailable(int reason) {
+                Toast.makeText(MapActivity.this, "not avail", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // move the camera to singapore
+        LatLng singapore = new LatLng(1.3521, 103.8198);
+        mMap.addMarker(new MarkerOptions().position(singapore).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(singapore));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(10));
+    }
+}
